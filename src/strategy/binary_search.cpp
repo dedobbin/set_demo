be0771e92adf1e4021fc1e8a819271e47d86e14f
dedobbin <you@example.com>
2023-11-20 19:36:00 +0100

    binary search strat actually uses binary search now

--- src/strategy/binary_search.cpp
+++ src/strategy/binary_search.cpp
@@ -18,7 +18,7 @@ void BinarySearch::add(std::vector<std::string> &list, std::string element) cons
 
 int BinarySearch::find(const std::vector<std::string> &list, std::string element) const
 {
-    std::cout << "Binary search TODO: use binary search" << std::endl;
-    auto it = std::find(list.begin(), list.end(), element);
-    return it != list.end() ? std::distance(list.begin(), it) : -1;
+    //bool found = std::binary_search(list.begin(), list.end(), element);
+    auto it = std::lower_bound(list.begin(), list.end(), element);
+    return it != list.end()  && *it == element ? std::distance(list.begin(), it) : -1;
 }
